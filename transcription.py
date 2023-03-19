e457d57926f7d0413a79d8862abb84203e1561b9
Adama <a.traore.pro@gmail.com>
2023-03-19 01:44:18 +0100

    Possibilité de transcrire de gros fichiers

--- transcription.py
+++ transcription.py
@@ -1,4 +1,5 @@
 import os
+import math
 os.environ["IMAGEIO_FFMPEG_EXE"] = "/opt/homebrew/bin//ffmpeg"
 import speech_recognition as sr
 import moviepy.editor as mp
@@ -10,28 +11,47 @@ video_name = input("Entrez le nom de la vidéo (sans extension): ")
 video_path = os.path.join("downloads", f"{video_name}.mp4")
 audio_path = os.path.join("downloads", f"{video_name}.wav")
 
-
-# Extraction de l'audio à partir de la vidéo
+# Extraire la durée totale de la vidéo
 clip = mp.VideoFileClip(video_path)
-clip.audio.write_audiofile(audio_path)
+duration = clip.duration
 
+# Calculer le nombre de segments de 10 Mo maximum nécessaires pour la vidéo
+max_segment_size = 10 * 1024 * 1024
+num_segments = int(math.ceil(os.path.getsize(video_path) / max_segment_size))
 
-# Vérifier si la vidéo existe
-if not os.path.isfile(video_path):
-    print("La vidéo n'existe pas")
-    exit()
+# Découper la vidéo en segments de 10 Mo maximum
+for i in range(num_segments):
+    segment_start = (i * duration) / num_segments
+    segment_end = ((i + 1) * duration) / num_segments
+    segment_path = os.path.join("downloads", f"{video_name}_{i}.mp4")
+    segment_clip = clip.subclip(segment_start, segment_end)
+    segment_clip.write_videofile(segment_path, codec='libx264', audio_codec='aac')
 
-# Initialiser le recognizer
-r = sr.Recognizer()
+    # Extraire l'audio de chaque segment de vidéo
+    segment_audio_path = os.path.join("downloads", f"{video_name}_{i}.wav")
+    segment_clip.audio.write_audiofile(segment_audio_path)
 
-# Ouvrir la vidéo en tant que source audio
-with sr.AudioFile(audio_path) as source:
-    # Analyser le fichier audio
-    audio_text = r.recognize_google(r.record(source), language="fr-FR")
+    # Transcrire le texte de chaque segment de vidéo
+    r = sr.Recognizer()
+    with sr.AudioFile(segment_audio_path) as source:
+        audio_text = r.recognize_google(r.record(source), language="fr-FR")
 
-# Écrire le texte transcrit dans un fichier texte
-output_file_path = os.path.join("downloads", f"{video_name}.txt")
-with open(output_file_path, "w") as file:
-    file.write(audio_text)
+    # Écrire le texte transcrit dans un fichier texte
+    output_file_path = os.path.join("downloads", f"{video_name}_{i}.txt")
+    with open(output_file_path, "w") as file:
+        file.write(audio_text)
 
-print("La transcription est terminée. Le texte transcrit est enregistré dans le fichier :", output_file_path)
+# Fusionner les fichiers de texte transcrits en un seul fichier
+with open(os.path.join("downloads", f"{video_name}.txt"), "wb") as outfile:
+    for i in range(num_segments):
+        segment_output_path = os.path.join("downloads", f"{video_name}_{i}.txt")
+        with open(segment_output_path, "rb") as segment_file:
+            outfile.write(segment_file.read())
+
+# Supprimer les fichiers temporaires
+for i in range(num_segments):
+    os.remove(os.path.join("downloads", f"{video_name}_{i}.mp4"))
+    os.remove(os.path.join("downloads", f"{video_name}_{i}.wav"))
+    os.remove(os.path.join("downloads", f"{video_name}_{i}.txt"))
+
+print("La transcription est terminée. Le texte transcrit est enregistré dans le fichier :", os.path.join("downloads", f"{video_name}.txt"))
